56a978f5fa8edf4d0c3fba710406ea688f45f53e
JHSaint <jordan.hewitta7@gmail.com>
2025-04-21 11:40:25 -0500

    Initial Commit for Project

---
+++ src/main/java/com/me/PawneeParksAndRecreationApplication.java
@@ -0,0 +1,13 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawneeParksAndRecreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawneeParksAndRecreationApplication.class, args);
+	}
+
+}
