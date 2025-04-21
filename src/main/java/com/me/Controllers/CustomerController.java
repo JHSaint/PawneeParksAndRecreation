2e7b2ab5d3ebace3ca832d6456b18d029c0dbba1
JHSaint <jordan.hewitta7@gmail.com>
2025-04-21 11:50:10 -0500

    Completed Customer Controller and added to file structure service and model packages and classes

---
+++ src/main/java/com/me/Controllers/CustomerController.java
@@ -0,0 +1,36 @@
+package com.me.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping(value = "/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping
+    public ArrayList<Customer> getAllCustomers(){return customerService.getAllCustomers();}
+
+    @GetMapping("{email}")
+    public Customer getCustomer(@PathVariable String email) {return customerService.getCustomer();}
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody CreateCustomerRequest request){
+        return customerService.createCustomer(request);
+    }
+
+    @DeleteMapping("delete/{email}")
+    public  ResponseEntity<String> deleteCustomer(@PathVariable String email){
+        return customerService.deleteCustomer(email);
+    }
+
+    @GetMapping
+    public String getCustomerInsight(@PathVariable String email){
+        return customerService.getInsight(email);
+    }
+}
